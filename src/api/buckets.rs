6efaa84c9501cdb134a9ae66af785d248d43f5d4
Armin Primadi <armin@dexcode.com>
2022-08-26 13:11:37 +0700

    Adding list all buckets and delete bucket API

--- src/api/buckets.rs
+++ src/api/buckets.rs
@@ -1,11 +1,45 @@
 //! Buckets API
 
-use crate::models::PostBucketRequest;
-use crate::{Client, Http, RequestError, ReqwestProcessing, Serializing};
 use reqwest::Method;
+use serde::{Serialize, Deserialize};
 use snafu::ResultExt;
 
+use crate::models::{Buckets, PostBucketRequest};
+use crate::{Client, Http, RequestError, ReqwestProcessing, Serializing};
+
 impl Client {
+    /// List all buckets matching specified parameters
+    pub async fn list_buckets(
+        &self,
+        request: Option<ListBucketsRequest>,
+    ) -> Result<Buckets, RequestError> {
+        let qs = serde_qs::to_string(&request).unwrap();
+        let url = match &qs[..] {
+            "" => format!("{}/api/v2/buckets", self.url),
+            _  => format!("{}/api/v2/buckets?{}", self.url, qs),
+        };
+
+        let response = self
+            .request(Method::GET, &url)
+            .send()
+            .await
+            .context(ReqwestProcessing)?;
+
+        if !response.status().is_success() {
+            let status = response.status();
+            let text = response.text().await.context(ReqwestProcessing)?;
+            let res = Http { status, text }.fail();
+            return res;
+        }
+
+        let res = response
+            .json::<Buckets>()
+            .await
+            .context(ReqwestProcessing)?;
+
+        Ok(res)
+    }
+
     /// Create a new bucket in the organization specified by the 16-digit
     /// hexadecimal `org_id` and with the bucket name `bucket`.
     pub async fn create_bucket(
@@ -32,6 +66,47 @@ impl Client {
 
         Ok(())
     }
+
+    /// Delete a bucket specified by bucket id.
+    pub async fn delete_bucket(
+        &self, 
+        bucket_id: &str
+    ) -> Result<(), RequestError> {
+        let url = format!("{}/api/v2/buckets/{}", self.url, bucket_id);
+        let response = self
+            .request(Method::DELETE, &url)
+            .send()
+            .await
+            .context(ReqwestProcessing)?;
+        if !response.status().is_success() {
+            let status = response.status();
+            let text = response.text().await.context(ReqwestProcessing)?;
+            Http { status, text }.fail()?;
+        }
+        Ok(())
+    }
+}
+ 
+/// Request for list buckets API
+#[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListBucketsRequest {
+    /// The last bucket ID from which to seek from (but not including). This
+    /// is to be used instead of `offset`.
+    pub after: Option<String>,
+    /// Only returns buckets with a specific ID.
+    pub id: Option<String>,
+    /// Number of buckets to return. Default: 20. Valid values: [1..100]
+    pub limit: Option<u8>,
+    /// Only returns buckets with a specific name.
+    pub name: Option<String>,
+    /// Offset from which to return buckets.
+    pub offset: Option<u64>,
+    /// The name of the organization.
+    pub org: Option<String>,
+    #[serde(rename = "orgID")]
+    /// The organization ID.
+    pub org_id: Option<String>,
 }
 
 #[cfg(test)]
@@ -63,6 +138,12 @@ mod tests {
             .await;
 
         mock_server.assert();
+    }
+
+    #[test]
+    fn serialize_empty_list_buckets_request() {
+        let request: Option<ListBucketsRequest> = None;
+        assert_eq!(serde_qs::to_string(&request).unwrap(), "");
     }
 }
 
