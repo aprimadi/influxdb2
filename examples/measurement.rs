3ecab785c35a758995c942c35c71a9c3b646896d
Armin Primadi <armin@dexcode.com>
2023-03-27 00:33:37 +0700

    Added measurement query examples

---
+++ examples/measurement.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let org = "sahamee";
+    let bucket = "bucket";
+    let influx_url = "http://localhost:8086";
+    let token = std::env::var("INFLUXDB2_TOKEN").unwrap();
+
+    let client = influxdb2::Client::new(influx_url, org, token);
+
+    let measurements = client.list_measurements(bucket).await.unwrap();
+    println!("measurements: {:?}", measurements);
+
+    for m in measurements.iter() {
+        let field_keys = client.list_measurement_field_keys(bucket, &m).await
+            .unwrap();
+        println!("field keys: {:?}", field_keys);
+    }
+
+    for m in measurements.iter() {
+        let tag_values = client
+            .list_measurement_tag_values(bucket, &m, "host")
+            .await;
+        println!(
+            "tag values for measurement {} and tag {}: {:?}",
+            &m, "host", tag_values
+        );
+    }
+
+    Ok(())
+}
