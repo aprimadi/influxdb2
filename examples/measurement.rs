091127f3afd96b6aeb0170fa9825d5966a12cb02
Armin Primadi <armin@dexcode.com>
2024-01-30 19:35:42 +0700

    Add optional `start` and `stop` parameters to schema query API

--- examples/measurement.rs
+++ examples/measurement.rs
@@ -9,12 +9,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let client = influxdb2::Client::new(influx_url, org, token);
 
-    let measurements = client.list_measurements(bucket, Some(365)).await.unwrap();
+    let measurements = client
+        .list_measurements(bucket, Some("-365d"), Some("-1d"))
+        .await
+        .unwrap();
     println!("measurements: {:?}", measurements);
 
     for m in measurements.iter() {
         let field_keys = client
-            .list_measurement_field_keys(bucket, &m, Some(365))
+            .list_measurement_field_keys(bucket, &m, Some("-365d"), Some("now()"))
             .await
             .unwrap();
         println!("field keys: {:?}", field_keys);
@@ -22,7 +25,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     for m in measurements.iter() {
         let tag_values = client
-            .list_measurement_tag_values(bucket, &m, "host", Some(365))
+            .list_measurement_tag_values(bucket, &m, "host", Some("-365d"), None)
             .await;
         println!(
             "tag values for measurement {} and tag {}: {:?}",
@@ -32,7 +35,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     for m in measurements.iter() {
         let tag_values = client
-            .list_measurement_tag_keys(bucket, &m, Some(365))
+            .list_measurement_tag_keys(bucket, &m, Some("-365d"), None)
             .await;
         println!(
             "tag values for measurement {} and tag {}: {:?}",
